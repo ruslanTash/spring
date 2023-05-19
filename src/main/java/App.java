1f226b1dcd0fd5c4c562a55403f67a65d2fa0265
ruslanTash <ruslan--t@yandex.ru>
2023-05-19 22:11:51 +0400

    задачи выполнены

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,20 @@
+import config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import pojo.Bus;
+import pojo.Car;
+import pojo.Driver;
+import pojo.Pickup;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("hi");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        context.getBean(Bus.class).getStart();
+        context.getBean(Car.class).getStart();
+        context.getBean(Pickup.class).getStart();
+
+        System.out.println(context.getBean("driverCar"));
+        System.out.println(context.getBean("driverBus"));
+        System.out.println(context.getBean("driverPickup"));
     }
 }
